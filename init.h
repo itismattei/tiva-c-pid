cd0eee0457026ccc655bb8d308e1fb87d23667a7
massimo <massimo@localhost>
2015-03-05 13:49:00 +0100

    Inseriti icomandi per il calcolo dei tokens

--- init.h
+++ init.h
@@ -12,12 +12,26 @@
 #define 	DIM_READ_BUFF		16
 #define		CHECK_SUM			0xA9
 
+//#############
+//valore dei tokens
+
+#define		ERRORE				255
+#define		AVANTI				1
+#define		INDIETRO			2
+#define		STOP				0
+#define		DESTRA				3		//(+90°)
+#define		SINISTRA			4		//(-90°)
+#define		GIRA_INDIETRO		5		//(180°)
+#define		GIRA_GRADI			6
+
+
 ///
 /// struttura che analizza la frase e ne mantiene il token del comando
 typedef struct _syn_stat{
 	int ST;
 	uint8_t cmd[2];
 	uint8_t l_cmd;
+	uint8_t token;
 } syn_stat;
 
 void parse(syn_stat *);
--- parse.c
+++ parse.c
@@ -10,7 +10,11 @@
 
 extern volatile uint8_t uart1buffer[16], RX_PTR1, READ_PTR1;
 
-
+void resetAutoma(syn_stat * STATO){
+	STATO->ST = 0;
+	STATO->cmd[0] = STATO->cmd[1] = 0;
+	STATO->l_cmd = 0;
+}
 
 ///
 /// analizza il comando che e' arrivato
@@ -27,6 +31,11 @@ void parse(syn_stat *STATO){
 			STATO->l_cmd = 2;
 			STATO->ST = 1;
 		}
+		else if(STATO->cmd[0] <= 16){
+			/// comando di richiesta dati lungo due caratteri
+			STATO->l_cmd = 2;
+			STATO->ST = 1;
+		}
 	break;
 
 	case 1:
@@ -35,18 +44,55 @@ void parse(syn_stat *STATO){
 			if (STATO->cmd[0] ^ CHECK_SUM == STATO->cmd[1]){
 				// ok
 				convertToToken(STATO);
+				// resetta l'automa
+				resetAutoma(STATO);
 			}
 		}
 
 	break;
 
 	}
-
 }
 
 
-
+///
+/// converte l'indicatoredi uncomand in un token
 void convertToToken(syn_stat *STATO){
+
+	switch(STATO->cmd[0]){
+	case 'A':
+		STATO->token = AVANTI;
+	break;
+	case 'I':
+		STATO->token = INDIETRO;
+	break;
+	case 'S':
+		STATO->token = STOP;
+	break;
+	case 'R':
+		STATO->token = DESTRA;
+	break;
+	case 'L':
+		STATO->token = SINISTRA;
+	break;
+	case 'B':
+		STATO->token = GIRA_INDIETRO;
+	break;
+	case 'G':
+		//// non ancora implementato
+		STATO->token = ERRORE;
+	break;
+	default:
+		/// se nessun comando e'giusto produce un errore.
+		STATO->token = ERRORE;
+	break;
+	}
+
+	/// potrebbe essere un comando nnuerico di richiesta dati
+	if (STATO->cmd[0] <= 16){
+
+	}
+
 }
 
 
