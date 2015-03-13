5d158dc9e3b253e3272d090ce175a02a5c12f202
robotics itismattei <robotics.itismattei@gmail.com>
2015-03-14 00:04:53 +0100

    conversione uscita PID verso valore del PWM

--- pid.h
+++ pid.h
@@ -26,6 +26,7 @@ typedef struct _pid{
 void PID(int valFin, gyro *G, pid *C);
 /// calcola il valore da fornire al PWM
 void setPWM(pid *C, pwm *P);
-
+/// imposta i coefficienti del PID
+void setKpid(pid *C, float kp, float kd, float ki);
 
 #endif /* PID_H_ */
